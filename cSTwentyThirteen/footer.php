758385d989a1086460d1493b6509498ea6ab282a
Christian M. Schmid <chms@gmx.net>
2013-07-19 21:08:30 +0200

    Fixed paths

--- cSTwentyThirteen/footer.php
+++ cSTwentyThirteen/footer.php
@@ -13,7 +13,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-		    <img src="wp-content/themes/cStwentytwelve/images/cS.png" alt="cS" /><br>
+		    <img src="wp-content/themes/cSTwentyThirteen/images/cS.png" alt="cS" /><br>
 		    &copy; <?php echo date('Y'); ?> Christian M. Schmid <br>
 			<?php do_action( 'twentytwelve_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>">Powered by WordPress</a>
